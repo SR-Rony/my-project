a0681d18e0f868429e273d77b6617c890b2f1d0c
SR-Rony <srrony707@gmail.com>
2023-12-25 11:26:16 +0600

    nevber responsive and img change

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from '../container/Container'
 import List from '../list/List'
 import ListItem from '../list/ListItem'
@@ -9,13 +9,24 @@ import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
     const [menu,setMenu]=useState(true)
+    useEffect(()=>{
+        const scroolWidth =()=>{
+            if(window.innerWidth < 768){
+                setMenu(false)
+            }else{
+                setMenu(true)
+            }
+        }
+        scroolWidth()
+        window.addEventListener("resize",scroolWidth)
+    },[]);
 
 
   return (
     <nav className='fixed top-0 left-0 z-50 w-full bg-slate-900 border-b border-primary'>
         <Container>
             <div className=" md:grid grid-cols-12 items-center md:py-3 relative">
-                <FaBars onClick={()=>setMenu(!menu)} className='absolute md:hidden top-5 right-0 cursor-pointer text-white text-xl'/>
+                <FaBars onClick={()=>setMenu(!menu)} className='absolute md:hidden top-8 right-3 cursor-pointer text-white text-xl'/>
                 <div className="col-span-2">
                     <div className='w-20 h-20 rounded-full cursor-pointer overflow-hidden'>
                         <Images src={Logo} alt='logo'/>
@@ -24,12 +35,12 @@ const Navbar = () => {
                 </div>
                 <div className="col-span-10 justify-center grid md:justify-end pb-4 md:pb-0">
                     {menu &&
-                        <List className='md:flex gap-5 text-xl font-semibold cursor-pointer text-white delay-500'>
-                        <ListItem className='hover:text-primary delay-100' text='Home' path='#home'/>
-                        <ListItem className='hover:text-primary delay-100' text='Abou Us' path='#about'/>
-                        <ListItem className='hover:text-primary delay-100' text='Service' path='#service'/>
-                        <ListItem className='hover:text-primary delay-100' text='Portfolio' path='#portfolio'/>
-                        <ListItem className='hover:text-primary delay-100' text='Contact' path='#contact'/>
+                    <List className='md:flex pt-3 md:pt-0 gap-5 text-xl font-semibold cursor-pointer text-white delay-500 '>
+                        <ListItem className='hover:text-primary delay-100 py-1 md:py-0' text='Home' path='#home'/>
+                        <ListItem className='hover:text-primary delay-100 py-1 md:py-0' text='Abou Us' path='#about'/>
+                        <ListItem className='hover:text-primary delay-100 py-1 md:py-0' text='Service' path='#service'/>
+                        <ListItem className='hover:text-primary delay-100 py-1 md:py-0' text='Portfolio' path='#portfolio'/>
+                        <ListItem className='hover:text-primary delay-100 py-1 md:py-0' text='Contact' path='#contact'/>
                     </List>
                     }
                 </div>
