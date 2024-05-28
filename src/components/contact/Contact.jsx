41b2a7712a05dfa238c2056f67fa502edb54afd9
SR Rony <146630569+SR-Rony@users.noreply.github.com>
2024-05-28 23:29:04 +0600

    Email js add

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Container from '../container/Container'
 import Button from '../button/Button'
 import Heading from '../heading/Heading'
@@ -6,8 +6,29 @@ import { MdLocationPin,MdOutlineMarkEmailRead  } from "react-icons/md";
 import { FaPhoneAlt,FaFacebookF,FaWhatsapp,FaGithub,FaLinkedinIn  } from "react-icons/fa";
 import Paragraph from '../paragraph/Paragraph';
 import { Link } from 'react-router-dom';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_if8mjs9', 'template_35iey9q', form.current, {
+        publicKey: 'gtcNriQF9jeNq3Fig',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
     <div id='contact' className='py-10 md:py-20'>
         <Container>
@@ -53,14 +74,15 @@ const Contact = () => {
                 </div>
                 <div className="col-span-3 md:col-span-2">
                     <Heading className='text-xl md:text-3xl text-white mb-5' text='Send us a note'/>
-                    <form className='text-white text-base md:text-xl' action="">
+                    <form ref={form} onSubmit={sendEmail} className='text-white text-base md:text-xl' action="">
                             <div className='flex mb-4 md:mb-7 gap-3 md:gap-5'>
-                                <input className='p-2 md:p-5 ring-primary ring bg-transparent  w-1/2' type="text" name='name' placeholder='Full Name' />
-                                <input className='p-2 md:p-5 ring-primary ring bg-transparent  w-1/2' type="text" name='name' placeholder='Email Address' />
+                                <input className='p-2 md:p-5 ring-primary ring bg-transparent  w-1/2' type="text" name="user_name" placeholder='Full Name' />
+                                <input className='p-2 md:p-5 ring-primary ring bg-transparent  w-1/2' type="text" name="user_email" placeholder='Email Address' />
                             </div>
-                            <textarea className='w-full bg-transparent p-2 md:p-5 h-32 md:h-52 ring ring-primary' name="textarea" placeholder='Tell us more about your needs ...'></textarea>
+                            <textarea className='w-full bg-transparent p-2 md:p-5 h-32 md:h-52 ring ring-primary' name="message"  placeholder='Tell us more about your needs ...'></textarea>
                             <div className='text-center mt-5'>
                                 <Button text='Send Message'/>
+                                {/* <input type="submit" value="Send" /> */}
                             </div>
                         </form>
                 </div>
--- src/components/copyright/Copyright.jsx
+++ src/components/copyright/Copyright.jsx
@@ -7,7 +7,7 @@ const Copyright = () => {
     <footer className='bg-black py-10'>
         <Container>
             <div className='md:flex items-center justify-between text-white text-center'>
-                <p className='font-poppins text-base md:text-xl font-medium mb-3 md:mb-0'>Copyright © 2023 <span className='text-primary'>SR Rony</span>. All Rights Reserved.</p>
+                <p className='font-poppins text-base md:text-xl font-medium mb-3 md:mb-0'>Copyright © 2024 <span className='text-primary'>SR Rony</span>. All Rights Reserved.</p>
                 <div className='flex gap-5 md:gap-10 items-center justify-center'>
                     <Paragraph text='Terms & Policy'/>
                     <Paragraph text='Disclaimer'/>
